83f74848d6506880d62c3200356c749707c5f6e8
Naara Souza <102614662+NaaraSouza@users.noreply.github.com>
2022-05-13 17:21:37 -0300

    Add files via upload

---
+++ TabelaDinamicaNovo2/src/main/java/br/com/senai/naarasouza/application/model/Despesas.java
@@ -0,0 +1,50 @@
+package br.com.senai.naarasouza.application.model;
+
+import java.util.Date;
+
+import javax.inject.Named;
+
+
+
+public class Despesas {
+	
+	private String data; // As letras do tipos Primitivos são maiúscula, pq elas aceitam valores nulos.
+	private String descricao;
+	private Double valor;
+	private boolean edit;
+	
+	
+	public String getData() {
+		return data;
+	}
+	public void setData(String data) {
+		this.data = data;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public Double getValor() {
+		return valor;
+	}
+	public void setValor(Double valor) {
+		this.valor = valor;
+	}
+	public boolean isEdit() {
+		return edit;
+	}
+	public void setEdit(boolean edit) {
+		this.edit = edit;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}

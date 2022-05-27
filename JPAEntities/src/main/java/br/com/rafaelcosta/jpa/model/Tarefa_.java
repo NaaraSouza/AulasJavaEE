44bd5f734b885e3113166248b92dea611ce3db92
Naara Souza <102614662+NaaraSouza@users.noreply.github.com>
2022-05-27 17:26:09 -0300

    Add files via upload

---
+++ JPAEntities/src/main/java/br/com/rafaelcosta/jpa/model/Tarefa_.java
@@ -0,0 +1,14 @@
+package br.com.rafaelcosta.jpa.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2022-05-27T16:38:43.194-0300")
+@StaticMetamodel(Tarefa.class)
+public class Tarefa_ {
+	public static volatile SingularAttribute<Tarefa, Integer> id;
+	public static volatile SingularAttribute<Tarefa, String> descricao;
+	public static volatile SingularAttribute<Tarefa, Integer> prioridade;
+	public static volatile SingularAttribute<Tarefa, Boolean> concluida;
+}

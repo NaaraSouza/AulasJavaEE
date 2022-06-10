54148403692861053182b0b376d5e00c6803e95d
Naara Souza <102614662+NaaraSouza@users.noreply.github.com>
2022-06-10 08:41:13 -0300

    Update FormatterBean.java

--- PedidoRelacionamentoJPA/src/main/java/br/com/rafaelcosta/application/bean/FormatterBean.java
+++ PedidoRelacionamentoJPA/src/main/java/br/com/rafaelcosta/application/bean/FormatterBean.java
@@ -15,6 +15,7 @@ public class FormatterBean implements Serializable {
 	private static final Locale LOCALE_BR = new Locale("pt", "BR"); // O Locale ele entrega 2 informaçõe, A lingua- e o Lugar(ex:"pt", "BR")
 	
 	public String formatarMoeda(double valor) {
+		//NumberFormat formata números. A classe realiza a distinção entre o sinal de ponto, milhar e de decimal, também identifica a posição do sinal do número e identifica o prefixo que indica a moeda em caso de valores monetários.
 		NumberFormat nf = NumberFormat.getCurrencyInstance(LOCALE_BR);//getCurrencyInstance-"pega a instância da moeda"
 		return nf.format(valor);// vai retornar o valor com o R$
 	}

d4974ce18ad2352491f3216f38f1fb32747915ed
Naara Souza <102614662+NaaraSouza@users.noreply.github.com>
2022-06-10 09:07:26 -0300

    Update Tarefa_.java

--- JPAEntities/src/main/java/br/com/rafaelcosta/jpa/model/Tarefa_.java
+++ JPAEntities/src/main/java/br/com/rafaelcosta/jpa/model/Tarefa_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
 @Generated(value="Dali", date="2022-05-27T16:38:43.194-0300")
-@StaticMetamodel(Tarefa.class)
+@StaticMetamodel(Tarefa.class) //permite gerar e referenciar estaticamente os atributos de cada entidade do seu modelo.
 public class Tarefa_ {
 	public static volatile SingularAttribute<Tarefa, Integer> id;
 	public static volatile SingularAttribute<Tarefa, String> descricao;
